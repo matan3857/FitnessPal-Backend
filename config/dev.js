f6dfdb50628cf89cfeb6ed78c623298e16d517ae
matan3857 <matan38355@gmail.com>
2022-05-28 20:24:29 +0300

    update

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://matanle1994:3yh8vU1wUL385dWu@cluster0.rnsyd.mongodb.net/?retryWrites=true&w=majority',
-  // 'dbURL': 'mongodb://localhost:27017',
+  // 'dbURL': 'mongodb+srv://matanle1994:3yh8vU1wUL385dWu@cluster0.rnsyd.mongodb.net/?retryWrites=true&w=majority',
+  'dbURL': 'mongodb://localhost:27017',
 }
