0e02b254641195c07d8eb5c3a17947ade1c12d33
Matan Levi <matan38355@gmail.com>
2022-11-12 10:25:24 +0200

    check

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
     // Configuring CORS
     const corsOptions = {
         // Make sure origin contains the url your frontend is running on
-        origin: ['https://fitnesspal-backend.onrender.com', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000', 'https://localhost:3000'],
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000', 'https://localhost:3000'],
         credentials: true
     }
     app.use(cors(corsOptions))
